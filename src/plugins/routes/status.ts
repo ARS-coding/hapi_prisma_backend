436e7406858d64bb4e993b32584989b58e1e0e69
Ali Rıza Şahin <arsahin.dev@gmail.com>
2024-07-28 20:19:16 +0300

    extract route definitions into a variable for status.ts

--- src/plugins/routes/status.ts
+++ src/plugins/routes/status.ts
@@ -1,16 +1,20 @@
-import Hapi from "@hapi/hapi";
+import Hapi, { ServerRoute } from "@hapi/hapi";
+
+const STATUS_ROUTES: ServerRoute[] = [
+  {
+    method: ["GET"],
+    path: "/status",
+    handler: (req: Hapi.Request, h: Hapi.ResponseToolkit) => {
+      return h.response({ up: true }).code(200);
+    },
+  },
+];
 
 export const statusPlugin: Hapi.Plugin<undefined> = {
-  name: "app/status",
+  name: "status",
   register: async function (server: Hapi.Server) {
     try {
-      server.route({
-        method: "GET",
-        path: "/status",
-        handler: (_, h: Hapi.ResponseToolkit) => {
-          return h.response({ up: true }).code(200);
-        },
-      });
+      server.route(STATUS_ROUTES);
     } catch (err) {
       console.error("An error occurred while registering status plugin: ", err);
     }
