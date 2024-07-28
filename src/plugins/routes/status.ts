58a12fe8ec370afdd13b986fc46d7f67685df41f
Ali Rıza Şahin <arsahin.dev@gmail.com>
2024-07-28 16:57:49 +0300

    add error handling to status plugin

--- src/plugins/routes/status.ts
+++ src/plugins/routes/status.ts
@@ -3,12 +3,16 @@ import Hapi from "@hapi/hapi";
 export const statusPlugin: Hapi.Plugin<undefined> = {
   name: "app/status",
   register: async function (server: Hapi.Server) {
-    server.route({
-      method: "GET",
-      path: "/status",
-      handler: (_, h: Hapi.ResponseToolkit) => {
-        return h.response({ up: true }).code(200);
-      },
-    });
+    try {
+      server.route({
+        method: "GET",
+        path: "/status",
+        handler: (_, h: Hapi.ResponseToolkit) => {
+          return h.response({ up: true }).code(200);
+        },
+      });
+    } catch (err) {
+      console.error("An error occurred while registering status plugin: ", err);
+    }
   },
 };
