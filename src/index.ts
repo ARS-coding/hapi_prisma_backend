459446c097f3db380a872fa073acf2072340f59e
Ali Rıza Şahin <arsahin.dev@gmail.com>
2024-07-28 15:24:00 +0300

    add status endpoint through registering it as a plugin to hapi

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,36 @@
 "use strict";
 
 import Hapi from "@hapi/hapi";
-
-async function init() {
-  const server: Hapi.Server = Hapi.server({
-    port: process.env.PORT || 3000,
-    host: process.env.HOST || "localhost",
-  });
-  await server.start();
-}
+import { statusPlugin } from "./plugins/status";
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
 
-init()
-  .then(() => console.log("Server started running!"))
-  .catch((err) => console.error("An error occured while initializing the HTTP server: ", err));
+const server = Hapi.server({
+  port: process.env.PORT || 3000,
+  host: process.env.HOST || "localhost",
+});
+
+async function registerServerPlugins() {
+  server.register([statusPlugin]);
+  return server;
+}
+
+async function startServer() {
+  await server.start();
+  return server;
+}
+
+(async function main() {
+  try {
+    await registerServerPlugins();
+    console.log("Server plugins registered!");
+
+    await startServer();
+    console.log("Server started running!");
+  } catch (err) {
+    console.error("An error occured while initializing the HTTP server: ", err);
+  }
+})();
