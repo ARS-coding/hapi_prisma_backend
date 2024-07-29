16064b4df9a69570c338ec968bd3fb28061fa836
Ali Rıza Şahin <arsahin.dev@gmail.com>
2024-07-29 18:51:45 +0300

    remove unnecessary comments and unused variables

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -1,7 +1,6 @@
-import Hapi, { Request, ResponseToolkit } from "@hapi/hapi";
+import { Request, ResponseToolkit } from "@hapi/hapi";
 
 import * as PostService from "../services/post";
-// use joi to validate the schema, look into doing that in the route creating option part instead of on the controller
 
 export async function postPost(req: Request, h: ResponseToolkit) {
   type ExpectedPayload = { title: string; content: string };
--- src/services/favorite.ts
+++ src/services/favorite.ts
@@ -1,5 +1,4 @@
 import * as FavoriteDataAccess from "../data-access/favorite";
-// use joi to validate the schema, look into doing that in the route creating option part instead of on the controller
 
 export function favorite(post_id: string, user_id: string) {
   //   return FavoriteDataAccess.createFavorite;
